9f7856d3f27ae52d1c99c2b29905aae3b83b8e0c
Niamot Ullah <niamotullah009@gmail.com>
2025-03-01 18:06:58 +0600

    404 issue

--- File/taskBoard.js
+++ File/taskBoard.js
@@ -1,6 +1,6 @@
 // Discover section added 
 document.querySelector('#taskAssigned2').addEventListener('click',function(){
-    window.location.href = '../HTML/Blogs.html'
+    window.location.href = '../HTML/main.html'
 })
 
 
